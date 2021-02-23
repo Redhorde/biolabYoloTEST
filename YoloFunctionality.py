a5369da347e2e92ceacf93de707b35f2aa44a88f
Redhorde <rdhorde.ent@gmail.com>
2021-02-23 17:41:22 +0100

    Add files via upload
    Initial release

---
+++ YoloFunctionality.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+import os
+
+
+def yolo_cut(image):
+    net = cv2.dnn.readNet("hand-obj_final.weights", "hand-obj.cfg")
+    classes = []
+    with open("obj.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    layer_names = net.getLayerNames()
+    outputlayers = [layer_names[i[0]-1]for i in net.getUnconnectedOutLayers()]
+
+    #img = cv2.imread("hand.JPG")
+    img = image
+    height, width, channels = img.shape
+    #cv2.imshow("Hand", img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outputs = net.forward(outputlayers)
+    #print(outputs)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outputs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+                #cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 1)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.6)
+    font = cv2.FONT_HERSHEY_PLAIN
+    print("Hand bounding box:")
+    print(boxes)
+    x = boxes[0][0]
+    y = boxes[0][1]
+    w = boxes[0][2]
+    h = boxes[0][3]
+    #print(str(x)+" "+str(y)+" "+str(w)+" "+str(h))
+    img_crop = img[y-50:y+h+50, x-50:x+w+50]
+    # cv2.imshow("Hand_cropped", img_crop)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    cv2.imwrite("hand_crop.JPG", img_crop)
+    return img_crop
+
+
+def vgg_detect():
+    pass
+
+
+def main():
+    for folder in os.scandir("Dataset"):
+        for file in os.listdir(folder.path):
+            pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ YoloTest.py
@@ -0,0 +1,49 @@
+from fastai1.old.fastai.conv_learner import *
+from fastai1.old.fastai.model import *
+import cv2
+
+import os
+import pandas as pd
+import numpy as np
+import re
+import scipy
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+import YoloFunctionality
+from YoloFunctionality import yolo_cut
+
+
+files_path = "zbiory/valid/fist/emg-03_PK-sequential-2018-03-27-12-12-46-342.mov-8907-fist.png"
+vgg_arch = resnet34
+vgg_sz = 300
+vgg_data = ImageClassifierData.from_paths('./vgg', tfms=tfms_from_model(vgg_arch, vgg_sz), bs=128)
+vgg_learn = ConvLearner.pretrained(vgg_arch, vgg_data, precompute=False)
+vgg_learn.load('best_sgdr_v3')
+vgg_result = pd.Series()
+
+image = cv2.imread(files_path)
+img_crop = yolo_cut(image)
+img_crop = image
+#y0 = 200
+#x0 = 500
+#height = 600
+#width = 600
+#image = image[y0:y0 + height, x0:x0 + width]  # crop
+img_crop = img_crop.astype(np.float32) / 255
+img_crop = cv2.cvtColor(img_crop, cv2.COLOR_BGR2RGB)
+img_crop = cv2.resize(img_crop, (600, 600))
+
+
+trn_tfms, val_tfms = tfms_from_model(vgg_arch, vgg_sz)
+im = val_tfms(img_crop)
+
+predictions = vgg_learn.predict_array(im[None])
+res = np.argmax(predictions)
+print("predictions:")
+print(predictions)
+print("argmax(predictions):")
+print(res)
+plt.imshow(img_crop)
+plt.show()
